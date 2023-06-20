6392a4e756925ce1f59702946c9335318066d0e1
kleines Filmröllchen <filmroellchen@serenityos.org>
2023-06-20 21:59:40 +0200

    Improve note envelope to something piano-like

--- praeludium.c
+++ praeludium.c
@@ -53,39 +53,43 @@ Note that C# is not used, and could be elided.
 */
 
 // gcc praeludium.c -o praeludium && ./praeludium | aplay -f cd
-static double ff[] = {110.0,116.5,123.5,130.8,138.6,146.8,155.6,164.8,
-    174.6,185.0,196.0,207.7};
-static char const* n1 = "@DGLP@BINQ?BGNQ@DGLP@DIPU@BFIN?BGNS?@DGL=@DGL"
-    "6=BFL;?BGK;>DGM9=BIN9<BEK8;@GL89=@E69=@E/6;?E48;@D4;>@D-9=@D.4=@C"
-    "09?@B/9=?B/8;@D/6;@E/6;?E/7=@F/8;@G/6;@E/6;?E(4;>D";
-static char const* n2 = "(49=@E@=@=9=9696(3GKNQNKNKGKBEDB(4DGLGD3";
-static void p(int n) {
+static double ff[] = { 110.0, 116.5, 123.5, 130.8, 138.6, 146.8, 155.6, 164.8,
+    174.6, 185.0, 196.0, 207.7 };
+static char const* n1 = "8<?DH8:AFI7:?FI8<?DH8<AHM8:>AF7:?FK78<?D58<?D.A:>D3C:?C36<?E15:AF"
+                        "14:=C038?D0158=.158='.37=,038<,368<%158<&,58;(178:'157:'038<'.38="
+                        "'.37='/58>'038?'.38='.37= ,36<";
+static char const* n2 = " ,158=8585151.1. +?CFIFCFC?C:=<: ,<?D?<,0,0,,,,,";
+static void p(int n)
+{
     double f = ff[n % 12];
-    while (n >= 12) { n -= 12; f *= 2; }
+    while (n >= 12) {
+        n -= 12;
+        f *= 2;
+    }
     for (int k = 0; k < 9000; ++k) {
         double ph = k * f / 44100;
         int v = (int)((ph - (int)ph) * 16384);
-        if (v > 8192) v = 16384 - v;
-        if (k < 400) v = (int)(v * k / 400);
-        if (k > 8000) v = (int)(v * (9000 - k) / 1000);
-        putchar(v & 255); putchar((v >> 8) & 255);
-        putchar(v & 255); putchar((v >> 8) & 255);
+        // Modify v sometimes:
+        if (v > 8192)
+            v = 16384 - v;
+        if (k < 4000)
+            v = (int)(v * (1 + (4000. - k) / 4000.));
+        if (k < 100)
+            v = (int)(v * k / 100);
+        if (k > 8000)
+            v = (int)(v * (9000 - k) / 1000);
+        putchar(v & 255);
+        putchar((v >> 8) & 255);
+        putchar(v & 255);
+        putchar((v >> 8) & 255);
     }
 }
-static void p2() {
-    for (int k = 0; k < 72000; ++k) {
-        double ph = k / 200.4545;
-        int v = (int)((ph - (int)ph) * 16384);
-        if (v > 8192) v = 16384 - v;
-        if (k < 400) v = (int)(v * k / 400);
-        if (k > 9000) v = (int)(v * (72000 - k) / 63000);
-        putchar(v & 255); putchar((v >> 8) & 255);
-        putchar(v & 255); putchar((v >> 8) & 255);
-    }
-}
-int main() {
+// Main entry point:
+int main()
+{
     for (int i = 0; i < strlen(n1) / 5; ++i)
         for (int j = 0; j < 16; ++j)
-            p(n1[i * 5 + (j % 8) % 5] - 40);
-    for (int i = 0; i < strlen(n2); ++i) p(n2[i] - 40); p2();
+            p(n1[i * 5 + (j % 8) % 5] - 29);
+    for (int i = 0; i < strlen(n2); ++i)
+        p(n2[i] - 29);
 }
